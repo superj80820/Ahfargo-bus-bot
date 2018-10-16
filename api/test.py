d8e8e0264153d8d2bda807d3dd3657d152e23267
exosite-yorklin <yorklin@gmail.com>
2018-10-16 23:49:38 +0800

    Add nearby stop file

--- api/test.py
+++ api/test.py
@@ -1,42 +1,38 @@
 # coding: utf-8
-import json
-from math import sin, cos, sqrt, atan2, radians
-
-def detection_distance(center, around, max_distance):
-    # approximate radius of earth in km
-    R = 6373.0
-
-    lat1 = radians(24.162015)
-    lon1 = radians(120.699098)
-    lat2 = radians(float(around['StopPosition']['PositionLat']))
-    lon2 = radians(float(around['StopPosition']['PositionLon']))
-
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-    distance = R * c
-
-    if distance < max_distance:
-        return [around['StopName']['Zh_tw'], around['StopID'], distance]
+# 0: 去程, 1: 返程
+from setting import *
 
 ans = []
 
-with open('res/stop.json','rb') as f:
+with open('res/stop.json','r') as f:
     data = json.load(f)
 
 for item in data:
-    temp = detection_distance(item, item, 0.5)
+    temp = common().detection_distance(item, item, 0.5)
     if temp != None:
         ans += [temp]
 print(ans)
 
-check = input('請輸入查詢公車號')
-with open('res/test.json','rb') as f:
+# lat = str(24.162015)
+# lon = str(120.699098)
+# max_distance = str(500)
+# headers=motc().get_headers()
+# res=requests.get('http://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/Taichung?$select=StopName&$spatialFilter=nearby(StopPosition, %s, %s, %s)&$format=JSON' % (lat, lon, max_distance),headers=headers)
+# print(res.json())
+# print(len(res.json()))
+
+ans = {}
+ans['direction_0']=[]
+ans['direction_1']=[]
+
+check = "三川福德祠"
+with open('res/test.json','r') as f:
     data = json.load(f)
 for item in data:
-    if item['StopID']==check:
-        print(item['RouteID'],item['Direction'])
+    if item["StopName"]["Zh_tw"] == check:
+        if item['Direction'] == 0:
+            ans['direction_0'] += [{'StopID': item['StopID'], 'RouteID': item['RouteID'], 'Direction': item['Direction']}]
+        elif item['Direction'] == 1:
+            ans['direction_1'] += [{'StopID': item['StopID'], 'RouteID': item['RouteID'], 'Direction': item['Direction']}]
+print(ans)
 
