eef7c57f60a87ef0df27f738498ffa5a1acbe8e5
York Lin <superj80820@gmail.com>
2018-10-24 22:49:30 +0800

    新增動態腳踏車網頁

--- web/assets/js/bike.js
+++ web/assets/js/bike.js
@@ -1,100 +1,71 @@
+window.onload = function (e) {
+	getQueryVariable("pos",GetBikeInfo)
+}
 
+function GetBikeInfo(pos,local_pos,callback){
+	$.ajax({
+		type: 'GET',
+		url: 'https://messfar.com/Ahfargo_bus_bot_staging_free_api/bike?pos='+pos,
+		dataType: 'json',
+		success: function(dict) {
+			console.log(dict)
+			callback(local_pos,dict);
+		}
+	});
+}
 
-//init
-var mapObj = new GMaps({
-	zoom:18,
-	el: "#map",
-	lat: 22.6134681,
-	lng: 120.3041978
-});
+function initbike(local_pos,dict){
+	//init
+	var mapObj = new GMaps({
+		zoom:15,
+		el: "#map",
+		lat: parseFloat(local_pos[0]),
+		lng: parseFloat(local_pos[1])
+	});
 
-//pokemons
-var pokemons = {
-	pokemon1:{
-		lat: 22.614189, 
-		lng: 120.305586,  
-		content:'2016/10/16 10:00',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	},
-	pokemon2:{
-		lat: 22.613889, 
-		lng: 120.305586,  
-		content:'2016/10/16 10:01',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	},
-	pokemon3:{
-		lat: 22.613789, 
-		lng: 120.305286,  
-		content:'2016/10/16 10:02',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	},
-	pokemon4:{
-		lat: 22.613789, 
-		lng: 120.304886,  
-		content:'2016/10/16 10:03',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	},
-	pokemon5:{
-		lat: 22.613589, 
-		lng: 120.304586,   
-		content:'2016/10/16 10:04',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	},
-	pokemon6:{
-		lat: 22.613589, 
-		lng: 120.304086,   
-		content:'2016/10/16 10:05',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	},
-	pokemon7:{
-		lat: 22.613489, 
-		lng: 120.303886,   
-		content:'2016/10/16 10:06',	  
-		image:'./images/bike liff.png',
-		title:'pikachu'
-	}
+	mapObj.addMarker({
+		lat: parseFloat(local_pos[0]),
+		lng: parseFloat(local_pos[1]),
+		title: 'dict[dict_index].StationAddress.Zh_tw',
+		icon:{url: './images/pikachu.png',scaledSize: new google.maps.Size(80, 80)},
+		infoWindow: {
+			content: '你的位置~呱'
+		},
+		click: function(e) {
+			console.log("you click:"+'dict[dict_index].title');
+		},
+		animation: google.maps.Animation.DROP
+	});		
 
-};
-
-
-
-var timeout = 1;
-for (var pokemon in pokemons) {	
-	// 立即函式(IIFE) Immediately Invoked Function Expression
-	(function(index,pokemon) {
-        setTimeout(function() { 
-			var img = pokemons[pokemon].image;
-			var icon = {
-			  url: img,
-			  scaledSize: new google.maps.Size(80, 80) 
-			};
+	var timeout = 1;
+	for (var dict_index in dict) {	
+		// 立即函式(IIFE) Immediately Invoked Function Expression
+		(function(index,dict_index) {
+			setTimeout(function() { 
+				var img = './images/bike liff.png';//dict[dict_index].image;
+				var icon = {
+				url: img,
+				scaledSize: new google.maps.Size(80, 80) 
+				};
+				
+				// Create marker
+				mapObj.addMarker({
+					lat: dict[dict_index].StationPosition.PositionLat,
+					lng: dict[dict_index].StationPosition.PositionLon,
+					title: dict[dict_index].StationAddress.Zh_tw,
+					icon:icon,
+					infoWindow: {
+						content: dict[dict_index].StationAddress.Zh_tw+'<br/>'+'可租借數量：'+dict[dict_index].AvailableRentBikes+'<br/>'+'可歸還數量：'+dict[dict_index].AvailableReturnBikes
+					},
+					click: function(e) {
+						console.log("you click:"+'dict[dict_index].title');
+					},
+					animation: google.maps.Animation.DROP
+				});		
 			
-			// Create marker
-			mapObj.addMarker({
-				lat: pokemons[pokemon].lat,
-				lng: pokemons[pokemon].lng,
-				title: pokemons[pokemon].title,
-				icon:icon,
-				infoWindow: {
-					content: pokemons[pokemon].content,
-				},
-				click: function(e) {
-					console.log("you click:"+pokemons[pokemon].title);
-				 },
-				animation: google.maps.Animation.DROP
-			});		
+			}, timeout * 100);
+		})(timeout,dict_index);
 		
-		}, timeout * 100);
-    })(timeout,pokemon);
-	
-	timeout++;
- }
-
- 
- 
+		timeout++;
+	}
+}
