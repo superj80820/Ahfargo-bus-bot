48c1b87e7182ea5703f3ed953fc6ce80370f2ea1
York Lin <superj80820@gmail.com>
2018-10-26 22:49:07 +0800

    Add keyboardpreview file

--- web/assets/js/lib.js
+++ web/assets/js/lib.js
@@ -1,35 +1,35 @@
 function getQueryVariable_bike(variable,callback)
 {
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){
-                var local_pos = pair[1].split("and")
-                // return pair[1];
-                // alert(pair[1]);
-                callback(pair[1],local_pos,initbike)
-            }
-       }
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){
+            var local_pos = pair[1].split("and")
+            // return pair[1];
+            // alert(pair[1]);
+            callback(pair[1],local_pos,initbike)
+        }
+    }
     // alert('false');
     //return(false);
 }
 
 function getQueryVariable_bus(variable,callback)
 {
-       var query = window.location.search.substring(1);
-    //    alert(query)
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){
-                // return pair[1];
-                console.log(query)
-                // alert(pair[1]);
-                // callback(,)
-                return callback(pair[1])
-            }
-       }
+    var query = window.location.search.substring(1);
+//    alert(query)
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){
+            // return pair[1];
+            console.log(query)
+            // alert(pair[1]);
+            // callback(,)
+            return callback(pair[1])
+        }
+    }
     // alert('false');
     //return(false);
 }
