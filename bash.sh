c997462b5ddc58e0422b12641257d228da7fa058
superj80820 <superj80820@gmail.com>
2018-11-25 10:00:56 +0000

    fix

--- bash.sh
+++ bash.sh
@@ -5,6 +5,6 @@ SCRIPTPATH=$(dirname "$ABSPATH") # /home/user/bin
 echo $SCRIPTPATH # /home/user/bin
 
 sudo chown www-data:www-data $SCRIPTPATH/api/__init__.py
-sudo chown www-data:www-data $SCRIPTPATH/api/data/db/user_action.db 
+sudo chown www-data:www-data $SCRIPTPATH/api/data/db/user_action.db
 sudo chown www-data:www-data $SCRIPTPATH/api/data/image
 
