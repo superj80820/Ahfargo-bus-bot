ce954549be08414577a69e48af47c485fc996066
superj80820 <superj80820@gmail.com>
2019-09-30 01:56:08 +0800

    test

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-    proxy: "http://app:5010/",
+    proxy: "http://app:8090/",
     disableHostCheck: true
   },
   publicPath: process.env.NODE_ENV === 'messfarDev'
